aa47b64197a416e69989eb686c4ef01909dc805d
ozwaldorf <self@ossian.dev>
2022-07-06 22:15:35 -0400

    feat: index best offers in makeOffer, cancelOffer, directBuy, and acceptOffer

--- src/ledger.rs
+++ src/ledger.rs
@@ -37,11 +37,19 @@ pub struct Event {
     pub seller: Option<Principal>,
 }
 
+#[derive(CandidType, Clone, Deserialize, Debug)]
+pub struct Offer {
+    pub buyer: Principal,
+    pub fungible: Principal,
+    pub price: Nat,
+}
+
 #[derive(Default, Clone, Deserialize, Debug, CandidType)]
 pub struct TokenData {
     pub id: String,
     pub traits: Option<HashMap<String, GenericValue>>,
     // pub events: Vec<Event>, // do we want to store txn history at all??
+    pub offers: Vec<Offer>,
     pub price: Option<Nat>,
     pub best_offer: Option<Nat>,
     pub last_listing: Option<Nat>,
@@ -64,6 +72,7 @@ impl Ledger {
         Ledger {
             nft_canister_id: Principal::management_canister(),
             custodians: vec![],
+            // define sort indexes to unwrap get_mut safely
             sort_index: HashMap::from([
                 ("listing_price".to_string(), vec![]),
                 ("offer_price".to_string(), vec![]),
@@ -94,7 +103,7 @@ impl Ledger {
         );
     }
 
-    fn _push_sort_offer(&mut self, token_id: String) {
+    fn push_sort_offer(&mut self, token_id: String) {
         let sorted = self.sort_index.get_mut("offer_price").unwrap();
         let mut db = self.db.clone();
 
@@ -164,12 +173,16 @@ impl Ledger {
             "makeOffer" => {
                 // update db entry
                 token.last_offer = Some(time.into());
-                token.best_offer = event.price;
-                // match event.buyer {
-                //     Some(buyer) => {
-                //         let offer = token.offers.entry(buyer.clone()).or_default();
-                //     }
-                // }
+                token.best_offer = event.price.clone();
+
+                token.offers.push(Offer {
+                    buyer: event.buyer.unwrap_or(Principal::management_canister()),
+                    fungible: event.nft_canister_id.clone(),
+                    price: event.price.unwrap_or(Nat::from(0)),
+                });
+
+                // index offer price
+                self.push_sort_offer(event.token_id.clone());
 
                 // TODO: offer price index
                 self.shift_or_push("last_offer", event.token_id.clone());
@@ -178,8 +191,50 @@ impl Ledger {
                 // TODO:
                 // remove from last offer index and offer price index if its the only one left (cancelled only offer)
                 // If not, leave it in the index, and re-sort the offer price index (cancelled offer but others remain)
+                match event.buyer {
+                    Some(buyer) => {
+                        token.offers.retain(|o| o.buyer != buyer);
 
-                self.remove("last_offer", event.token_id.clone());
+                        match &token.best_offer {
+                            Some(best_offer) => match token.offers.last() {
+                                Some(offer) => {
+                                    if offer.price.clone() > best_offer.clone() {
+                                        token.best_offer = Some(offer.price.clone());
+                                    }
+                                }
+                                None => {
+                                    token.best_offer = None;
+                                }
+                            },
+                            None => unreachable!(),
+                        }
+                    }
+                    None => {}
+                }
+
+                if token.offers.is_empty() {
+                    // remove from last offer and offer price indexes if no more offers on the token
+                    self.remove("last_offer", event.token_id.clone());
+                    self.remove("offer_price", event.token_id.clone());
+                } else {
+                    // re-sort offer price index
+
+                    // find best offer
+                    let mut best_offer = None;
+                    for offer in token.offers.iter() {
+                        if best_offer.is_none() {
+                            best_offer = Some(offer.price.clone());
+                        } else if offer.price.clone() > best_offer.clone().unwrap() {
+                            best_offer = Some(offer.price.clone());
+                        }
+                    }
+
+                    // update best offer
+                    token.best_offer = best_offer;
+
+                    // sort offer price index
+                    self.push_sort_offer(event.token_id.clone());
+                }
             }
 
             "directBuy" => {
@@ -187,7 +242,50 @@ impl Ledger {
                 token.price = None;
                 token.last_sale = Some(time.into());
 
-                // TODO: mirror offer removal logic if one exists for the buyer
+                match event.buyer {
+                    Some(buyer) => {
+                        token.offers.retain(|o| o.buyer != buyer);
+
+                        match &token.best_offer {
+                            Some(best_offer) => match token.offers.last() {
+                                Some(offer) => {
+                                    if offer.price.clone() > best_offer.clone() {
+                                        token.best_offer = Some(offer.price.clone());
+                                    }
+                                }
+                                None => {
+                                    token.best_offer = None;
+                                }
+                            },
+                            None => unreachable!(),
+                        }
+                    }
+                    None => {}
+                }
+
+                if token.offers.is_empty() {
+                    // remove from last offer and offer price indexes if no more offers on the token
+                    self.remove("last_offer", event.token_id.clone());
+                    self.remove("offer_price", event.token_id.clone());
+                } else {
+                    // re-sort offer price index
+
+                    // find best offer
+                    let mut best_offer = None;
+                    for offer in token.offers.iter() {
+                        if best_offer.is_none() {
+                            best_offer = Some(offer.price.clone());
+                        } else if offer.price.clone() > best_offer.clone().unwrap() {
+                            best_offer = Some(offer.price.clone());
+                        }
+                    }
+
+                    // update best offer
+                    token.best_offer = best_offer;
+
+                    // sort offer price index
+                    self.push_sort_offer(event.token_id.clone());
+                }
 
                 // remove from listing index
                 self.remove("listing_price", event.token_id.clone());
@@ -200,6 +298,50 @@ impl Ledger {
                 token.last_sale = Some(time.into());
 
                 // TODO: mirror offer removal logic
+                match event.buyer {
+                    Some(buyer) => {
+                        token.offers.retain(|o| o.buyer != buyer);
+
+                        match &token.best_offer {
+                            Some(best_offer) => match token.offers.last() {
+                                Some(offer) => {
+                                    if offer.price.clone() > best_offer.clone() {
+                                        token.best_offer = Some(offer.price.clone());
+                                    }
+                                }
+                                None => {
+                                    token.best_offer = None;
+                                }
+                            },
+                            None => unreachable!(),
+                        }
+                    }
+                    None => {}
+                }
+
+                if token.offers.is_empty() {
+                    // remove from last offer and offer price indexes if no more offers on the token
+                    self.remove("last_offer", event.token_id.clone());
+                    self.remove("offer_price", event.token_id.clone());
+                } else {
+                    // re-sort offer price index
+
+                    // find best offer
+                    let mut best_offer = None;
+                    for offer in token.offers.iter() {
+                        if best_offer.is_none() {
+                            best_offer = Some(offer.price.clone());
+                        } else if offer.price.clone() > best_offer.clone().unwrap() {
+                            best_offer = Some(offer.price.clone());
+                        }
+                    }
+
+                    // update best offer
+                    token.best_offer = best_offer;
+
+                    // sort offer price index
+                    self.push_sort_offer(event.token_id.clone());
+                }
 
                 // remove from listing index
                 self.remove("listing_price", event.token_id.clone());
--- test/test.sh
+++ test/test.sh
@@ -6,7 +6,7 @@ dfx deploy curation --argument '(null)'
 
 # Random traits
 traits=("Bronze" "Silver" "Gold" "Platinum" "Diamond")
-prices=(1 2 3 4 5)
+prices=(1 2 3 5 8 13 21 34 55 89 144) # lets get fibby!
 nft_canister_id="aaaaa-aa"
 
 echo "-> insert 'mint' events (tokens 0-14)..."
@@ -38,6 +38,8 @@ for page in {0..1}; do
   dfx canister call curation query "(\"all\", $page)"
 done
 
+
+
 echo "-> insert 'makeListing' events (tokens 0-1)"
 for i in {0..1}; do
   price=${prices[$((RANDOM % ${#prices[@]}))]}
@@ -53,20 +55,15 @@ for i in {0..1}; do
   )"
 done
 
-echo "-> query for tokens by 'last_listing'"
-for page in {0..1}; do
-  printf "  $page: "
-  dfx canister call curation query "(\"last_listing\", $page)"
-done
+echo "-> query for tokens by 'last_listing' (page 0)"
+dfx canister call curation query "(\"last_listing\", 0)"
+echo "-> query for tokens by 'listing_price' (page 0)"
+dfx canister call curation query "(\"listing_price\", 0)"
 
-echo "-> query for tokens by 'listing_price'"
-for page in {0..1}; do
-  printf "  $page: "
-  dfx canister call curation query "(\"listing_price\", $page)"
-done
 
-echo "-> insert 'makeOffer' events (tokens 2-3)"
-for i in {2..3}; do
+
+echo "-> insert 'makeOffer' events (tokens 2-4)"
+for i in {2..4}; do
   price=${prices[$((RANDOM % ${#prices[@]}))]}
   echo "make offer for token $i (price: $price)"
 
@@ -75,19 +72,56 @@ for i in {2..3}; do
       nft_canister_id=principal\"$nft_canister_id\";
       token_id=\"$i\";
       operation=\"makeOffer\";
+      buyer=opt principal\"$nft_canister_id\";
       price=opt($i);
     }
   )"
 done
 
-echo "-> query for tokens by 'last_offer'"
-for page in {0..1}; do
-  printf "  $page: "
-  dfx canister call curation query "(\"last_offer\", $page)"
-done
+echo "-> query for tokens by 'last_offer' (page 0)"
+dfx canister call curation query "(\"last_offer\", 0)"
+echo "-> query for tokens by 'offer_price' (page 0)"
+dfx canister call curation query "(\"offer_price\", 0)"
+
+
+
+echo "-> 'cancelOffer' for token 2"
+dfx canister call curation insert "(
+  record {
+    nft_canister_id=principal\"$nft_canister_id\";
+    token_id=\"2\";
+    operation=\"cancelOffer\";
+    buyer=opt principal\"$nft_canister_id\";
+  }
+)"
+
+echo "-> query for tokens by 'last_offer' (page 0)"
+dfx canister call curation query "(\"last_offer\", 0)"
+echo "-> query for tokens by 'offer_price' (page 0)"
+dfx canister call curation query "(\"offer_price\", 0)"
+
+
+
+echo "-> make additional offer to token 3"
+dfx canister call curation insert "(
+  record {
+    nft_canister_id=principal\"$nft_canister_id\";
+    token_id=\"3\";
+    operation=\"makeOffer\";
+    buyer=opt principal\"$nft_canister_id\";
+    price=opt(200);
+  }
+)"
+
+echo "-> query for tokens by 'last_offer' (page 0)"
+dfx canister call curation query "(\"last_offer\", 0)"
+echo "-> query for tokens by 'offer_price' (page 0)"
+dfx canister call curation query "(\"offer_price\", 0)"
+
+
 
 echo "-> query for all tokens"
-for page in {0..1}; do
+for page in {0..0}; do
   printf "  $page: "
   dfx canister call curation query "(\"all\", $page)"
 done
